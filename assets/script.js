835b672c0d7c70f4465c24b8becc8061e208b16c
Yuval Nahmias <yuvalit789@gmail.com>
2023-10-31 23:29:36 -0400

    event problem

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var timerEL = document.querySelector("#time");
 var questionEl = document.getElementById("game-section");
 var questionIndex = 0;
 
-var questions = [
+var questionList = [
   {
     question: "What is not a common data type?",
     choices: ["Strings", "Boleans", "alerts", "numbers"],
@@ -18,17 +18,17 @@ var questions = [
 ];
 // console.log(questions[1].choices[2]);
 
-// for (var i = 0; i < questions[questionIndex].choices.length; i++) {
+// for (var i = 0; i < questionList[questionIndex].choices.length; i++) {
 //   console.log(questions[questionIndex].choices[i]);
 // }
 function quiz() {
   questionEl.innerHTML = "";
-  // console.log(questions[questionIndex].question);
+  // console.log(questionList[questionIndex].question);
   // console.log(questionIndex);
   // questionIndex++;
 
   var question1 = document.createElement("h2");
-  question1.textContent = questions[questionIndex].question;
+  question1.textContent = questionList[questionIndex].question;
   //questionEl.textContent = document.body.appendChild(question1);
 
   var listEl = document.createElement("ol");
@@ -36,18 +36,26 @@ function quiz() {
   questionEl.appendChild(question1);
   questionEl.appendChild(listEl);
 
-  for (var i = 0; i < questions[questionIndex].choices.length; i++) {
+  for (var i = 0; i < questionList[questionIndex].choices.length; i++) {
     var li = document.createElement("li");
-    li.textContent = questions[questionIndex].choices[i];
+    li.textContent = questionList[questionIndex].choices[i];
     li.classList.add("li-class");
     listEl.appendChild(li);
     var liEl = document.querySelectorAll("li");
   }
 
-  function test() {
+  var answerChoice = document.querySelectorAll(".li-class");
+  function test(event) {
     console.log("test");
   }
-  listEl.addEventListener("click", test);
+  for (let i = 0; i < 4; i++) {
+    answerChoice[i].addEventListener("click", test);
+  }
+  li.addEventListener("click", test);
+
+  if ((event.target.textContent = questionList[questionIndex].answer)) {
+    console.log("yay");
+  }
 }
 
 //countdown function - a countdown on the top right side of the page to signify the number pf seconds left to finish the quiz.
