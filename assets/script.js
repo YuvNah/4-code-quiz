8c49a38ae563a25b33a0bba632211ef97c8eba12
Yuval Nahmias <yuvalit789@gmail.com>
2023-11-04 16:44:37 -0400

    broken question functionality

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,32 @@ var timerEL = document.querySelector("#time");
 var questionEl = document.getElementById("game-section");
 var questionIndex = 0;
 
-var questionList = [
-  {
-    question: "What is not a common data type?",
-    choices: ["Strings", "Boleans", "alerts", "numbers"],
-    answer: "Boleans",
-  },
-
-  {
-    question: "What is the capital of United States?",
-    choices: ["California", "New York", "Miami", "Florida"],
-    answer: "California",
-  },
-];
 // console.log(questions[1].choices[2]);
 
 // for (var i = 0; i < questionList[questionIndex].choices.length; i++) {
 //   console.log(questions[questionIndex].choices[i]);
 // }
 function quiz() {
+  var questionList = [
+    {
+      question: "What is not a common data type?",
+      choices: ["Strings", "Boleans", "alerts", "numbers"],
+      answer: "Boleans",
+    },
+
+    {
+      question: "What is the capital of United States?",
+      choices: ["California", "New York", "Miami", "Florida"],
+      answer: "California",
+    },
+
+    {
+      question: "another question",
+      choices: ["1", "2", "3", "4"],
+      answer: "4",
+    },
+  ];
+
   questionEl.innerHTML = "";
   // console.log(questionList[questionIndex].question);
   // console.log(questionIndex);
@@ -43,19 +50,30 @@ function quiz() {
     listEl.appendChild(li);
     var liEl = document.querySelectorAll("li");
   }
-
   var answerChoice = document.querySelectorAll(".li-class");
-  function test(event) {
+
+  // this function will call the next question after the user answered the last, regardless of right or wrong answer
+  function nextQuestion(event) {
     console.log("test");
+
+    for (var i = 0; i < questionList[questionIndex].question.length; i++) {
+      question1.textContent = questionList[i].question;
+    }
+    questionEl.appendChild(question1);
   }
   for (let i = 0; i < 4; i++) {
-    answerChoice[i].addEventListener("click", test);
+    answerChoice[i].addEventListener("click", nextQuestion);
   }
-  li.addEventListener("click", test);
+  // if correct answer add "corect" under the next question
+  //if wrong answer add "wrong" and deduct time off the timer
+  // when out of questions time = 0 and the time left = points
 
-  if ((event.target.textContent = questionList[questionIndex].answer)) {
-    console.log("yay");
-  }
+  // if ((event.target.textContent = questionList[questionIndex].answer)) {
+  //   console.log("yay");
+  // }
+  // }
+
+  // li.addEventListener("click", nextQuestion);
 }
 
 //countdown function - a countdown on the top right side of the page to signify the number pf seconds left to finish the quiz.
